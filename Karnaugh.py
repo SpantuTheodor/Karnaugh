efc81f67d0d0638aeff8cd018681daf974ed7b0c
SpantuTheodor <teospantu5@gmail.com>
2020-12-14 12:26:44 +0200

    Added functionality for 3 variable functions

--- Karnaugh.py
+++ Karnaugh.py
@@ -1,8 +1,16 @@
-truth_table = [[0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 1, 0, 0], [0, 0, 1, 1, 0], [0, 1, 0, 0, 0],
-               [0, 1, 0, 1, 0], [0, 1, 1, 0, 0],
-               [0, 1, 1, 1, 0], [1, 0, 0, 0, 0],
-               [1, 0, 0, 1, 0], [1, 0, 1, 0, 0], [1, 0, 1, 1, 0], [1, 1, 0, 0, 0], [1, 1, 0, 1, 0],
-               [1, 1, 1, 0, 0], [1, 1, 1, 1, 0]]
+def define_truth_table(number_of_variables):
+    if number_of_variables == 3:
+        return [[0, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 1, 1, 0], [1, 0, 0, 0], [1, 0, 1, 0], [1, 1, 0, 0],
+                [1, 1, 1, 0]]
+    elif number_of_variables == 4:
+        return [[0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 1, 0, 0], [0, 0, 1, 1, 0], [0, 1, 0, 0, 0],
+                [0, 1, 0, 1, 0], [0, 1, 1, 0, 0],
+                [0, 1, 1, 1, 0], [1, 0, 0, 0, 0],
+                [1, 0, 0, 1, 0], [1, 0, 1, 0, 0], [1, 0, 1, 1, 0], [1, 1, 0, 0, 0], [1, 1, 0, 1, 0],
+                [1, 1, 1, 0, 0], [1, 1, 1, 1, 0]]
+    else:
+        print("Syntax Error")
+        return False
 
 
 def validate_string(string_to_validate):
@@ -17,8 +25,14 @@ def validate_string(string_to_validate):
                 while string_to_validate[index + 1] in "0123456789":
                     number = number * 10 + int(string_to_validate[index + 1])
                     index += 1
-                if number > 15:
-                    return syntax_error()
+
+                if number_of_variables == 3:
+                    if number > 7:
+                        return syntax_error()
+                if number_of_variables == 4:
+                    if number > 15:
+                        return syntax_error()
+
                 modify_truth_table(number, 1)
             elif string_to_validate[index] not in ",":
                 return syntax_error()
@@ -39,8 +53,14 @@ def validate_string(string_to_validate):
                         while string_to_validate[index + 1] in "0123456789":
                             number = number * 10 + int(string_to_validate[index + 1])
                             index += 1
-                        if number > 15:
-                            return syntax_error()
+
+                            if number_of_variables == 3:
+                                if number > 7:
+                                    return syntax_error()
+                            if number_of_variables == 4:
+                                if number > 15:
+                                    return syntax_error()
+
                         modify_truth_table(number, '*')
                     elif string_to_validate[index] not in ",":
                         return syntax_error()
@@ -59,19 +79,31 @@ def syntax_error():
 
 
 def modify_truth_table(line_number, value):
-    truth_table[line_number][4] = value
+    if number_of_variables == 3:
+        truth_table[line_number][3] = value
+    else:
+        truth_table[line_number][4] = value
 
 
 def write_truth_table_in_file():
     f = open("truth_table.txt", "w")
-    f.write("No. A B C D O \n")
-    for index in range(len(truth_table)):
-        if int(index / 10) == 0:
-            index_to_string = " " + str(index)
-        else:
+    if number_of_variables == 3:
+        f.write("No. A B C O \n")
+        for index in range(len(truth_table)):
             index_to_string = str(index)
-        f.write(" " + index_to_string + " " + str(truth_table[index][0]) + " " + str(truth_table[index][1]) + " " + str(
-            truth_table[index][2]) + " " +
+            f.write(
+                " " + index_to_string + " " + str(truth_table[index][0]) + " " + str(truth_table[index][1]) + " " + str(
+                    truth_table[index][2]) + " " + str(truth_table[index][3]) + "\n")
+    else:
+        f.write("No. A B C D O \n")
+        for index in range(len(truth_table)):
+            if int(index / 10) == 0:
+                index_to_string = " " + str(index)
+            else:
+                index_to_string = str(index)
+            f.write(
+                " " + index_to_string + " " + str(truth_table[index][0]) + " " + str(truth_table[index][1]) + " " + str(
+                    truth_table[index][2]) + " " +
                 str(truth_table[index][3]) + " " + str(truth_table[index][4]) + "\n")
     fnc()
     f.close()
@@ -80,39 +112,69 @@ def write_truth_table_in_file():
 def fnc():
     condition = 0
     print("FNC: ", end='')
-    for index, line in enumerate(truth_table, start=0):
-        if line[4] == 0:
-            if index != 0:
-                print(" ∧ ", end='')
-            if line[0] == 0:
-                print("(A v ", end='')
-                condition = 1
-            else:
-                print("(!A v ", end='')
-                condition = 1
-            if line[1] == 0:
-                print("B v ", end='')
-                condition = 1
-            else:
-                print("!B v ", end='')
-                condition = 1
-            if line[2] == 0:
-                print("C v ", end='')
-                condition = 1
-            else:
-                print("!C v ", end='')
-                condition = 1
+    for line in truth_table:
+
+        if number_of_variables == 3:
             if line[3] == 0:
-                print("D)", end='')
-                condition = 1
-            else:
-                print("!D)", end='')
-                condition = 1
+                if condition != 0:
+                    print(" ∧ ", end='')
+                if line[0] == 0:
+                    print("(A v ", end='')
+                    condition = 1
+                else:
+                    print("(!A v ", end='')
+                    condition = 1
+                if line[1] == 0:
+                    print("B v ", end='')
+                    condition = 1
+                else:
+                    print("!B v ", end='')
+                    condition = 1
+                if line[2] == 0:
+                    print("C)", end='')
+                    condition = 1
+                else:
+                    print("!C)", end='')
+                    condition = 1
+
+        if number_of_variables == 4:
+            if line[4] == 0:
+                if condition != 0:
+                    print(" ∧ ", end='')
+                if line[0] == 0:
+                    print("(A v ", end='')
+                    condition = 1
+                else:
+                    print("(!A v ", end='')
+                    condition = 1
+                if line[1] == 0:
+                    print("B v ", end='')
+                    condition = 1
+                else:
+                    print("!B v ", end='')
+                    condition = 1
+                if line[2] == 0:
+                    print("C v ", end='')
+                    condition = 1
+                else:
+                    print("!C v ", end='')
+                    condition = 1
+                if line[3] == 0:
+                    print("D)", end='')
+                    condition = 1
+                else:
+                    print("!D)", end='')
+                    condition = 1
+
     if condition == 0:
         print("1")
     print("\n")
 
 
-input_string = str(input())
-validate_string(input_string)
-print(truth_table)
+print("Number of Variables: ")
+number_of_variables = int(input())
+truth_table = define_truth_table(number_of_variables)
+if truth_table:
+    input_string = str(input())
+    validate_string(input_string)
+    print(truth_table)
